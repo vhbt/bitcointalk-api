af4b73fc85e4c443cb759ef673924a0dd0046419
Vitor Hariel <vhbarauna@gmail.com>
2020-06-07 22:20:06 -0300

    1.4.0

--- src/app/controllers/TopicController.js
+++ src/app/controllers/TopicController.js
@@ -1,11 +1,35 @@
 require('dotenv').config();
+const { Op } = require('sequelize');
 const { Topic } = require('../models/Topic');
+const { Mention } = require('../models/Mention');
 
 class TopicController {
   async index(req, res) {
-    const topics = await Topic.findAndCountAll({ order: [['id', 'DESC']] });
+    const topics = await Topic.findAndCountAll({
+      order: [['id', 'DESC']],
+    });
 
-    return res.json(topics);
+    const topicsTracking = await Promise.all(
+      topics.rows.map(async (topic) => {
+        const tracking = await Mention.findAll({
+          where: {
+            chat_id: {
+              [Op.in]: topic.tracking,
+            },
+          },
+        });
+
+        return {
+          author: topic.author,
+          id: topic.id,
+          link: topic.link,
+          title: topic.title,
+          tracking,
+        };
+      })
+    );
+
+    return res.json(topicsTracking);
   }
 }
 
